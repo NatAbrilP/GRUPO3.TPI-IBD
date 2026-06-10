8d056bac19b13724a6f7b7e77511ebb465489a35
Mastersus <mateosarubi@gmail.com>
2026-06-10 02:15:02 -0300

    Adaptación DML
    Adaptación del DML final con la tabla de pedido_mesa.

--- Script 2 - DML.sql
+++ Script 2 - DML.sql
@@ -1,62 +1,75 @@
 use restaurante;
 
-insert into categoria(nombre) values
-	('Entradas'),
-    ('Platos Principales'),
-    ('Postres'),
-    ('Bebidas sin alcohol'),
-    ('Bebidas con alcohol');
+insert into categoria(id_categoria, nombre) values
+    (1, 'Entradas'),
+    (2, 'Platos Principales'),
+    (3, 'Postres'),
+    (4, 'Bebidas sin alcohol'),
+    (5, 'Bebidas con alcohol');
 
-insert into producto(nombre, precio, stock, id_categoria) values
-	('Empanada', 1400.00, 20, 1),
-	('Tabla de fiambres', 22000.00, 10, 1),
-    ('Provoleta', 15000.00, 15, 1),
-    ('Bife de chorizo', 35000.00, 12, 2),
-    ('Milanesa napolitana', 28000.00, 18, 2),
-    ('Pasta del día', 17000.00, 8, 2),
-    ('Salmón grillado', 30000.00, 6, 2),
-    ('Tiramisú', 8100.00, 10, 3),
-    ('Flan casero', 6300.00, 12, 3),
-    ('Helado', 5000.00, 20, 3),
-    ('Agua mineral 500ml', 3600.00, 50, 4),
-    ('Gaseosa 500ml', 4150.00, 40, 4),
-    ('Jugo de naranja', 3500.00, 15, 4),
-    ('Cerveza Quilmes', 10900.00, 30, 5),
-    ('Vino', 20000.00, 25, 5),
-    ('Jarra de Fernet', 28000.00, 10, 5);
+insert into producto(id_producto, nombre, precio, stock, id_categoria) values
+    (1, 'Empanada', 1400.00, 20, 1),
+    (2, 'Tabla de fiambres', 22000.00, 10, 1),
+    (3, 'Provoleta', 15000.00, 15, 1),
+    (4, 'Bife de chorizo', 35000.00, 12, 2),
+    (5, 'Milanesa napolitana', 28000.00, 18, 2),
+    (6, 'Pasta del día', 17000.00, 8, 2),
+    (7, 'Salmón grillado', 30000.00, 6, 2),
+    (8, 'Tiramisú', 8100.00, 10, 3),
+    (9, 'Flan casero', 6300.00, 12, 3),
+    (10, 'Helado', 5000.00, 20, 3),
+    (11, 'Agua mineral 500ml', 3600.00, 50, 4),
+    (12, 'Gaseosa 500ml', 4150.00, 40, 4),
+    (13, 'Jugo de naranja', 3500.00, 15, 4),
+    (14, 'Cerveza Quilmes', 10900.00, 30, 5),
+    (15, 'Vino', 20000.00, 25, 5),
+    (16, 'Jarra de Fernet', 28000.00, 10, 5);
 
-insert into mozo(nombre, apellido, estado) values
-	('Ana', 'Ramírez',  'activo'),
-    ('Luis', 'Fernández','activo'),
-    ('Valentina', 'Gómez', 'activo'),
-    ('Ezequiel', 'Zeballos', 'activo');
+insert into mozo(id_mozo, nombre, apellido, estado) values
+    (1, 'Ana', 'Ramírez',  'activo'),
+    (2, 'Luis', 'Fernández','activo'),
+    (3, 'Valentina', 'Gómez', 'activo'),
+    (4, 'Ezequiel', 'Zeballos', 'activo');
 
-insert into mesa(numero, capacidad) values
-	(1, 2),
-    (2, 4),
-    (3, 4),
-    (4, 6),
-    (5, 6),
-    (6, 4),
-    (7, 4),
-    (8, 8),
-    (9, 2),
-    (10, 10);
+insert into mesa(id_mesa, numero, capacidad) values
+    (1, 1, 2),
+    (2, 2, 4),
+    (3, 3, 4),
+    (4, 4, 6),
+    (5, 5, 6),
+    (6, 6, 4),
+    (7, 7, 4),
+    (8, 8, 8),
+    (9, 9, 2),
+    (10, 10, 10);
 
-insert into pedido(id_mesa, id_mozo, fechaYHora, estado_pedido) values
-	(1, 1, '2026-06-01 12:30:00', 'entregado'),
-    (1, 1, '2026-06-01 20:15:00', 'entregado'),
-    (2, 2, '2026-06-02 13:00:00', 'entregado'),
+insert into pedido(id_pedido, id_mozo, fechaYHora, estado_pedido) values
+    (1, 1, '2026-06-01 12:30:00', 'entregado'),
+    (2, 1, '2026-06-01 20:15:00', 'entregado'),
+    (3, 2, '2026-06-02 13:00:00', 'entregado'),
     (4, 3, '2026-06-02 13:10:00', 'entregado'),
-    (4, 3, '2026-06-02 20:45:00', 'cancelado'),
+    (5, 3, '2026-06-02 20:45:00', 'cancelado'),
     (6, 4, '2026-06-03 12:50:00', 'entregado'),
     (7, 1, '2026-06-03 13:05:00', 'en_preparacion'),
-    (2, 2, '2026-06-04 20:00:00', 'entregado'),
-    (9,3, '2026-06-05 12:30:00', 'pendiente'),
+    (8, 2, '2026-06-04 20:00:00', 'entregado'),
+    (9, 3, '2026-06-04 21:00:00', 'entregado'),
     (10, 4, '2026-06-05 12:45:00', 'pendiente');
 
+insert into pedido_mesa(id_pedido, id_mesa) values
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 4),
+    (5, 4),
+    (6, 6),
+    (7, 7),
+    (8, 2),
+    (9, 10),
+    (10, 6),
+    (10, 8);
+
 insert into detalle_pedido(id_pedido, id_producto, cantidad, precio_unitario) values
-	(1, 1, 1, 1400.00),
+    (1, 1, 1, 1400.00),
     (1, 5, 2, 28000.00),
     (1, 12, 2, 4150.00),
     (2, 3, 1, 15000.00),
@@ -101,5 +114,3 @@ update mozo set estado='inactivo' where id_mozo=4;
 update producto set precio=19000 where id_producto=15;
 
 delete from detalle_pedido where id_detalle=1 and id_pedido in(select id_pedido from pedido where estado_pedido='pendiente');
-
-delete from detalle_pedido where id_detalle=29 and id_pedido in(select id_pedido from pedido where estado_pedido='pendiente');
